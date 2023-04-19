2799583a4d087f43bf3b2bd19d46fb9874cf0df0
Victor Garcia Mestre <vgmestre@gmail.com>
2023-04-19 23:01:51 +0200

    codemirror setup

---
+++ packages/renderer/src/hooks/use-codemirror.ts
@@ -0,0 +1,67 @@
+import type React from 'react';
+import {useEffect, useState, useRef} from 'react';
+import {EditorState} from '@codemirror/state';
+import {EditorView} from '@codemirror/view';
+import {
+  highlightActiveLine,
+  highlightActiveLineGutter,
+  keymap,
+  lineNumbers,
+} from '@codemirror/view';
+import {defaultKeymap, history, historyKeymap} from '@codemirror/commands';
+import {
+  bracketMatching,
+  defaultHighlightStyle,
+  indentOnInput,
+  syntaxHighlighting,
+} from '@codemirror/language';
+import {javascript} from '@codemirror/lang-javascript';
+
+interface Props {
+  initialDoc: string;
+  onChange?: (state: EditorState) => void;
+}
+
+const useCodeMirror = <T extends Element>({
+  initialDoc,
+  onChange,
+}: Props): [React.MutableRefObject<T | null>, EditorView?] => {
+  const refContainer = useRef<T>(null);
+  const [editorView, setEditorView] = useState<EditorView>();
+
+  useEffect(() => {
+    if (!refContainer.current) return;
+
+    const startState = EditorState.create({
+      doc: initialDoc,
+      extensions: [
+        keymap.of([...defaultKeymap, ...historyKeymap]),
+        lineNumbers(),
+        highlightActiveLineGutter(),
+        history(),
+        indentOnInput(),
+        bracketMatching(),
+        syntaxHighlighting(defaultHighlightStyle, {fallback: true}),
+        highlightActiveLine(),
+        javascript(),
+        EditorView.lineWrapping,
+        EditorView.updateListener.of(update => {
+          if (update.changes) {
+            onChange && onChange(update.state);
+          }
+        }),
+      ],
+    });
+
+    const view = new EditorView({
+      state: startState,
+      parent: refContainer.current,
+    });
+
+    setEditorView(view);
+  }, [refContainer]);
+
+  return [refContainer, editorView];
+};
+
+export default useCodeMirror;
