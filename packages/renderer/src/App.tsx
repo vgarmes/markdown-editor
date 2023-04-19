03fdff43750d596a22d1ef850b708d7cb15cdb8e
Victor Garcia Mestre <vgmestre@gmail.com>
2023-04-19 22:12:25 +0200

    small fixes

--- packages/renderer/src/App.tsx
+++ packages/renderer/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './app.css';
 
-const App: React.FC = () => <div className="app"></div>;
+const App: React.FC = () => <div className="app">Hello World</div>;
 
 export default App;
